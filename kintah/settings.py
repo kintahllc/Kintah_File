c5cf2054abb8425c1e6fc2fc63e914d5d33d904a
abdurrahmanc-dev <abdurrahmanc@lambdacrunch.com>
2024-07-31 02:34:11 +0600

    bug fix try-009

--- kintah/settings.py
+++ kintah/settings.py
@@ -252,3 +252,23 @@ AUTH_USER_MODEL = 'account.User'
 #         },
 #     },
 # }
+
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'file': {
+#             'level': 'DEBUG',
+#             'class': 'logging.FileHandler',
+#             'filename': os.path.join(BASE_DIR, 'django_debug.log'),
+#         },
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['file'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#     },
+# }
